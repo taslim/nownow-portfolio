3bf96fbda9dbb58e3f00eca679993d19d0b495f5
taslim <9999802+taslim@users.noreply.github.com>
2025-03-24 01:06:52 +0000

    Add new project entry for USPS Mail Alerts in projects.js.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,11 @@
 export const projects = [
   {
+    title: "USPS Mail Alerts Apps Script",
+    description: "Automatically create Google Tasks reminders based on USPS Informed Delivery emails",
+    github: "https://github.com/taslim/usps-mail-alerts",
+    live: "https://github.com/taslim/usps-mail-alerts"
+  },
+  {
     title: "Fortune Cookie",
     description: "Get daily fortune cookies from a single tap",
     github: "https://github.com/taslim/fortunekeeper",
